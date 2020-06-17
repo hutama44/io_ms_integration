a2eac2005c6f631f1a5fdb6ddce0a975f70e6842
hutama44 <hutachan@yahoo.com>
2020-06-17 13:38:56 -0400

    clean code

--- io_integrator.py
+++ io_integrator.py
@@ -16,19 +16,25 @@ class color:
    UNDERLINE = '\033[4;37;48m'
    END = '\033[1;37;0m'
 
+#Getting values for IP address and VPR from command arguments
 
 ip = sys.argv[1]
 vpr = sys.argv[2]
 tio = TenableIO('', '')
 mn = []
 cv = []
+
+#List of "bad words" to make the exploit search more efficient
 words = ["Microsoft","Windows","Linux","Apache"]
 
+
+#Function to search the exploit by CVE in Exploitdb
 def openlink(cve):
     cve = cve.replace('CVE-','')
     url = "https://www.exploit-db.com/search?cve=" + cve
     webbrowser.open_new_tab(url)
 
+#Function to search the exploit by exploit name in Metasploit
 def openms(name):
     for a in words:
         name = name.replace(a,'')
@@ -40,37 +46,32 @@ def openms(name):
     tar.interact()
     tar.kill(1)
 
-#for asset in tio.workbenches.assets():
-#  pprint.pprint(asset)
-
+#Getting Metasploit Exploit Names from T.io
 print(color.BLUE + "(+)   Exploits available in Metasploit" + color.END)
 for vuln in tio.exports.vulns(severity=['critical'],cidr_range=ip,vpr_score={"gt":vpr}):
     if "metasploit_name" in vuln["plugin"]:
         mn.append(vuln["plugin"]["metasploit_name"])
-#        pprint.pprint(vuln["plugin"]["metasploit_name"])
+
 mn = list(dict.fromkeys(mn))
+
+#Asking the user if wants to do the search for each exploit
 for i in mn:
     response = input(color.RED + "(+) Do you want to chek MS for " + i + "? (y/n)" + color.END)
     if response == "y":
      openms(i)
-#pprint.pprint(mn)
+
+#Getting CVEs from T.io
 print(color.BLUE + "(+)  CVE to search in other tools" + color.END)
 for vuln in tio.exports.vulns(severity=['critical'],cidr_range=ip,vpr_score={"gt":vpr}):
     if "cve" in vuln["plugin"]:
         cv.extend(vuln["plugin"]["cve"])
+
 cv = list(dict.fromkeys(cv))
+
+#Asking the user if wants to do the search for each exploit
 for i in cv:
     response = input(color.RED + "(+) Do you want to check exploitdb for " + i + "? (y/n)" + color.END)
     if response == "y":
      openlink(i)
     if response == "k":
         break
-
-#pprint.pprint(cv)
-
-#https://www.exploit-db.com/search?cve=2018-8625
-#print("(+)   Vuln Names")
-#for vuln in tio.exports.vulns(severity=['critical'],cidr_range=ip,vpr_score={"gt":8}):
-#        pprint.pprint(vuln["plugin"]["name"])
-#vuln = tio.workbenches.asset_vuln_info(asset_id,19506)
-#pprint.pprint(vuln)
